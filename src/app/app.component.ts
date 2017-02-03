31d067a9e497ff0ae021c3acdf6c4add2e68143a
Christian Kirschberg <ckirschberg@gmail.com>
2017-02-03 12:55:13 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'app works! Yeah! Try again';
+  
+  public model = {
+    name: 'Tudor',
+    todos: [
+      { name: 'Buy Flowers', state: false },
+      { name: 'Buy Shampoo', state: false },
+      { name: 'Buy Milk', state: true },
+      { name: 'Buy Candy', state: false },
+      { name: 'Take over the world', state: false },
+    ]
+  };
+
+  incompleteTodos() : number {
+    let count: number = 0;
+    for(let i=0; i < this.model.todos.length; i++) {
+      if (!this.model.todos[i].state) {
+        count++;
+      }
+    }
+    return count;
+  }
+
 }
